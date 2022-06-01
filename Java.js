85ba9c797a0e2db837b35ba5b71be323886cd0dd
SharapovR <105370558+SharapovR@users.noreply.github.com>
2022-06-01 19:54:05 +0500

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,97 @@
+// 1/2.
+// Foydalanuvchidan yoshni so’raydigan dasturni tuzing. Yosh 50 dan y uqori bo’lsa, dastur “Siz kattasiz” deb chiqarib berishi kerak. Yosh 50 dan past bo’lsa “Siz yoshsiz” deb chiqarib berishi kerak.
+
+// let age = 40
+
+// if (age > 50) {
+//    console.log('Siz kattasiz')
+// } else {
+//    console.log('Siz yoshsiz')
+// }
+
+
+
+
+// (2)/11.
+// loop keyingi qadamga o'tsin agar i 5 ga teng bo'lsa
+
+// for (i = 0; i < 10; i++) {
+//    if (i == 5) {
+//       continue
+//    }
+//    console.log(i)
+// }
+
+
+
+
+// (3)/14 ??
+// loop ishlating (farqi yo'q qaysi) Konsolga chiqarish 10 11 9 12 8 13 7 14 6 15 5 16 4 17 3 18 2 19 1 20 0
+
+
+
+
+
+// (4)/22.
+
+// bar() funksiyasi bor u 3 ta sonni bir biriga ko'paytirib qaytarishi kerak
+// ushbu vazifani bajaruvchi kod yozing
+// html
+
+// function myfun() {
+//    let a = document.getElementById("number1").value;
+//    let b = document.getElementById("number2").value;
+//    let c = document.getElementById("number3").value;
+//    document.getElementById("result").innerHTML = a * b * c;
+// }
+
+
+// let total = 0;
+// for (let num = 1; num < 300; num *= 1) {
+//    total = total * num
+// }
+// console.log("The total is: " + total);
+
+// for (let i = 1; i < 10; i *= 1) {
+//    console.log(i);
+// }
+
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//    i += i;
+// }
+
+// console.log(i);
+
+// let a = 4
+// let b = 1
+// let c = 1
+
+
+// while (c <= a; b = b * c,
+//    ) {
+//    c = c + 1;
+// }
+// console.log(c);
+// let plus = true
+// let counter = 1
+// let num = 1
+
+// console.log(num);
+
+// for (let i = 1; i <= 300; i++) {
+//    if (plus) {
+//       num += counter
+//       console.log(num)
+//       counter += 1
+//    }
+// }
+// masala while dan foydalanib hisoblash 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * … * 300
+// let counter = 1
+
+// for (let i = 1; i <= 300; i++) {
+//    counter *= i
+//    document.write(counter + '<br>')
+
+// }
